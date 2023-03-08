a4e090c5d9a03b0b42c5f822dda2dc38b93894fc
ArinaKozh <arina.mayer66@mail.ru>
2023-03-09 02:51:27 +0300

    Задача 2

---
+++ task_2.java
@@ -0,0 +1,11 @@
+/*
+ * Вывести все простые числа от 1 до 1000
+ */
+
+public class task_2 {
+    public static void main(String[] args){
+        for (int i=1;i<=1000;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
